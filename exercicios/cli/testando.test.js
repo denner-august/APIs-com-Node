f087a1a4485fe112817634804b88475c599e58da
denner augusto <denner.augusto90@gmail.com>
2023-02-17 14:20:37 -0300

    adicionando exercicios javascript/node

---
+++ exercicios/cli/testando.test.js
@@ -0,0 +1,30 @@
+const { ok, deepEqual } = require('assert')
+
+const DEFAULT_ITEM_CADASTRAR = { "nome": "flash", "poder": "Speed", "id": 1 }
+
+const base = require('./database')
+const database = require('./database')
+
+
+describe("suite de manipulação de herois", () => {
+    before(async () => {
+        await database.Cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
+
+    it('deve pesquisar um heroi utilizando arquivos', async () => {
+        const esperado = DEFAULT_ITEM_CADASTRAR
+
+        const [result] = await base.listar(esperado.id)
+        deepEqual(esperado, result);
+
+    })
+
+    it('deve cadastrar um heroi, usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+
+        await base.Cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [actual] = await base.listar(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(actual, expected)
+    })
+})
+
