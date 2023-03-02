1f5c26a6161580b7936cc134265a1ebffdd79d5e
denner augusto <denner.augusto90@gmail.com>
2023-03-02 17:38:19 -0300

    criando class postgres

--- project/src/db/strategies/postgres.js
+++ project/src/db/strategies/postgres.js
@@ -1,8 +1,62 @@
 const iCrud = require('./interfaces/interfaceCrud')
+const Sequelize = require('sequelize')
 
 class Postgres extends iCrud {
     constructor() {
         super()
+        this._driver = null
+        this._herois = null
+        this._connect()
+    }
+
+    async IsConnected() {
+        try {
+            await this._driver.authenticate()
+            return true
+        } catch (error) {
+            console.error(error)
+            return false
+
+        }
+    }
+
+    async defineModel() {
+        this._herois = driver.define('herois',
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    required: true,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                nome: {
+                    type: Sequelize.STRING,
+                    required: true
+                },
+                poder: {
+                    type: Sequelize.STRING,
+                    required: true
+                }
+            },
+            {
+                tableName: "TB_HEROIS",
+                freezeTableNames: false,
+                timestamps: false
+            }
+        )
+        await this._herois.sync()
+    }
+
+    _connect() {
+        this._driver = new Sequelize(
+            'heroes', 'denner', 'denner12',
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorAliases: false
+            }
+        )
     }
 
     create(item) {
