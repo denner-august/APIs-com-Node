c20fb638171ffc185bad6318ce3f04116f0b2d2c
denner augusto <denner.augusto90@gmail.com>
2023-03-20 20:18:02 -0300

    Create mongodbStrategy.js

---
+++ project/src/test/mongodbStrategy.js
@@ -0,0 +1,14 @@
+// const assert = require('assert')
+// const mongoDb = require('../db/strategies/mongodb')
+// const Context = require('../db/strategies/base/contextStrategy')
+
+// const context = new Context(new mongoDb())
+
+// describe('mongoDb suite de teste', () => {
+//     it('verificando conexão', async () => {
+//         const result = await context.IsConnected()
+//         const expected = 'conectado'
+
+//         assert.deepEqual(result, expected)
+//     })
+// })
